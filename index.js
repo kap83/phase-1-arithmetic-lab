bd757251f3bf98ef5e8300a5587d3b82b1d7436e
Karen Polanco <polanco.k@gmail.com>
2022-11-05 15:32:26 -0400

    commit tree ec5def1dfc56ea3db1557e8c5263358489344e9c

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+//is an equation whose multiplied results will equal 62 
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;  
+console.log(multiply) 
+
+//generates a random integer greater than 0
+const random = Math.ceil(Math.random() * 10) +1 ; 
+console.log(random)
+//*remember* Math has to be capitalized for this method to work. 
+
+//is an equation that calculates a remainder that is equal to 4
+let num3 = 4;
+let num4 = 32;
+const mod = num3 % num4;
+console.log(mod)
+// 4 % 32 
+
+//will return 20 as the highest number in the set
+let max = Math.max(5, 10, 15, 20);
+console.log(max);
